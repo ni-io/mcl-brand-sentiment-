803b1d504dded8b0bd7de27113ed0756de6d5c20
nick italiano <nickitaliano.io@gmail.com>
2021-08-19 22:48:26 -0700

    test .travis

---
+++ src/settings.py
@@ -0,0 +1,6 @@
+TRACK_WORDS = ['test']
+TABLE_NAME = "test"
+TABLE_ATTRIBUTES = "id_str VARCHAR(255), created_at TIMESTAMP DEFAULT now(), text VARCHAR(255), \
+            polarity INT, subjectivity INT, user_created_at VARCHAR(255), user_location VARCHAR(255), \
+           user_description VARCHAR(255), user_followers_count INT, longitude FLOAT8, latitude FLOAT8, \
+         retweet_count INT, favorite_count INT"
